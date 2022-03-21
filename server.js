e5c0b8df99e9196af41e32347623349523c0e905
Josh Stotts <joshstotts@Joshs-MacBook-Pro.local>
2022-03-21 16:00:49 -0500

    heroku updates

--- server.js
+++ server.js
@@ -12,13 +12,7 @@ const app = express();
 require("dotenv").config();
 
 // config variables from .env
-const { MONGODB_URL, PORT, GOOGLE_CREDENTIALS } = process.env;
-
-// const serviceAccount = JSON.parse(GOOGLE_CREDENTIALS);
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-// });
+const { MONGODB_URL, PORT } = process.env;
 
 // connect to mongoDB
 mongoose.connect(MONGODB_URL, {
@@ -54,26 +48,6 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(async function (req, res, next) {
-//     try {
-//         const token = req.get("Authorization");
-//         if (!token) return next();
-
-//         const user = await admin.auth().verifyIdToken(token.replace("Bearer", ""));
-//         if (!user) throw new Error("something went wrong");
-
-//         req.user = user;
-//         next();
-//     } catch (error) {
-//         res.status(400).json(error);
-//     }
-// });
-
-// function isAuthenticated(req, res, next) {
-//     if (!req.user) return res.status(401).json({ message: "You must be logged in to add a movie"})
-//     next();
-// }
-
 // ROUTES
 
 //index
@@ -93,11 +67,6 @@ app.post("/movies", async (req, res) => {
         res.status(400).json(error);
     }
 });
-
-
-
-
-
 
 // listen
 app.listen(PORT, () => {
